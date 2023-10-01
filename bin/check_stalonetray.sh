fd47cca267aadf923c8b767b3a50902130eda09e
Jon Bradley <weatchu@gmail.com>
2023-10-01 06:05:01 -0800

    add toolbar

--- bin/check_stalonetray.sh
+++ bin/check_stalonetray.sh
@@ -6,5 +6,5 @@ then
     echo "is running"
 else
     echo "is not running, starting..."
-    stalonetray --transparent --vertical --geometry 31x1-5+5 --grow-gravity S --icon-gravity NE --icon-size=16 --kludges=force_icons_size
+    stalonetray --transparent --vertical --geometry 31x1-42+5 --grow-gravity S --icon-gravity NE --icon-size=16 --kludges=force_icons_size
 fi
