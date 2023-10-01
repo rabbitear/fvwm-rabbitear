8ab6735185868a49608998d1ae6114f21d1a3006
Jon Bradley <weatchu@gmail.com>
2023-10-01 03:36:05 -0800

    fix check_xdaliclock.sh

--- bin/check_xdaliclock.sh
+++ bin/check_xdaliclock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if pgrep -i x "xdaliclock" > /dev/null
+if pgrep -x "xdaliclock" > /dev/null
 then
     echo "is running"
 else
     echo "is not running, starting..."
-    exec xdaliclock -24 -builtin0 -transparent -memory high \
-        -nocycle -seconds -fg grey30 -geometry -5-5
+    xdaliclock -24 -builtin0 -transparent -memory high \
+        -nocycle -seconds -fg grey30 -geometry -5-5 &
 fi
